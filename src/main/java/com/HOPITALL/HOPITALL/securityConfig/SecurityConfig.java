1444d6d6d29824cc4d2444ef7e66c23c7b686a01
almahdi76 <almahdi.abusef@yahoo.com>
2022-07-24 17:46:04 +0200

    security

---
+++ src/main/java/com/HOPITALL/HOPITALL/securityConfig/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.HOPITALL.HOPITALL.securityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // auth.inMemoryAuthentication()
+        // .withUser("almahdi").password("{noop}1234").roles("USER","ADMIN")
+        // .and()
+        // .withUser("sef").password("{noop}123").roles("USER");
+        PasswordEncoder passwordEncoder=passwordEncoder();
+        String PWD=passwordEncoder.encode("123");
+        auth.inMemoryAuthentication()
+        .withUser("almahdi").password(passwordEncoder.encode("1234")).roles("USER","ADMIN")
+        .and()
+        .withUser("sef").password(PWD).roles("USER");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       
+        //http.formLogin().loginPage("/nomPg");
+        http.formLogin();
+        http.authorizeHttpRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    
+}
